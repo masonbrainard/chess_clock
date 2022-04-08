dfed5c85f466d24e65d9916d5f6c85c1c11fb2a5
Mason <masonbrainard@gmail.com>
2022-04-08 16:46:09 -0600

    v3 with correct code for boardv1

--- chess_clock.c
+++ chess_clock.c
@@ -5,13 +5,16 @@ int bits_pins[4] = {6, 7, 8, 5};
 
 int buzz_pin = 9;
 
-int P1_button = 10;
-int P2_button = 13;
+int P1_button = 13;
+int P2_button = 10;
 int next_button = 11;
 int pause_button = 12;
 
 int inc_i = 0;
 int inc = 0;
+int inc_time;
+
+int temp_time;
 
 unsigned long last_time = 0;
 unsigned long int P1_time;
@@ -21,19 +24,20 @@ bool is_no_disp;
 bool button_debounce;
 
 unsigned long int bd_time;
+int bd_dif = 100;
 
 unsigned long int conv[4] = {6000, 600, 100, 10};
 
 enum stateMachine {HOLD_TIME, SET_TIME, SET_READY, P1_TURN, P2_TURN};
 enum stateMachine gameState;
 
-int bits_bins[10][4] = {{0,0,0,0},
+int bits_bins[11][4] = {{0,0,0,0},
 {0,0,0,1},{0,0,1,0},{0,0,1,1},
 {0,1,0,0},{0,1,0,1},{0,1,1,0},
-{0,1,1,1},{1,0,0,0},{1,0,0,1}};
+{0,1,1,1},{1,0,0,0},{1,0,0,1},{1,1,1,1}};
 
 void setup(){
-  //Serial.begin(9600);
+  Serial.begin(9600);
   
   pinMode(bits_pins[0], OUTPUT);
   pinMode(bits_pins[1], OUTPUT);
@@ -61,233 +65,166 @@ void setup(){
 
   last_time = millis();
 
-  //Serial.print("Setup complete\n");
+  Serial.println("Setup complete");
 }
 
 void loop()
 {
+  //HOLD_TIME//
   if(gameState == HOLD_TIME)
   {
-     while(true)
-     {
-       display(P1_time);
-       display(P2_time);
-       res_disp();
-       
-       if(digitalRead(next_button) == HIGH || digitalRead(pause_button) == HIGH)
-       {
-          gameState = SET_TIME;
-          P1_time = 0;
-          P2_time = 0;
-          break;
-       }
-       if(digitalRead(P1_button) == HIGH || digitalRead(P2_button) == HIGH)
-       {
-          no_disp();
-          buzzer();
-          res_disp();
-       }
-     }
-     set_bd();
+    Serial.println("HOLD_TIME");
+    while(gameState == HOLD_TIME)
+    {
+      display(P1_time);
+      display(P2_time);
+      res_disp();
+
+      if(digitalRead(P1_button) == HIGH)
+      {
+        Serial.println("P1 BUTTON PRESSED");
+        buzzer();
+      }
+      if(digitalRead(P2_button) == HIGH)
+      {
+        Serial.println("P2 BUTTON PRESSED");
+        buzzer();
+      }
+      if(digitalRead(pause_button) == HIGH || digitalRead(next_button) == HIGH)
+      {
+        gameState = SET_TIME;
+      }
+    }
   }
+  //SET_TIME//
   if(gameState == SET_TIME)
   {
-     int t = 0;
-     int i = 0;
-
-     last_time = millis();
-     
-     while(true)
-     {
-        if(i < 4)
+    //SET P1/P2
+    while(true)
+    {
+      
+      display(P1_time);
+      display(P2_time);
+      res_disp();
+      is_no_disp = true;
+      
+      if(digitalRead(P1_button) == HIGH && !button_debounce)
+      {
+        P1_time += 10;
+        Serial.print("\nP1_Time = ");
+        Serial.print(P1_time);
+        set_bd(250);
+      }
+      if(digitalRead(P2_button) == HIGH && !button_debounce)
+      {
+        if(P1_time >= 10)
         {
-          if(millis() - last_time < 1000)
-          {
-            display((P1_time + (t * conv[i])));
-            display((P2_time + (t * conv[i])));
-            res_disp();
-          }
-          else if(millis() - last_time < 1500)
-          {
-            if(!is_no_disp)
-            {
-              no_disp();
-              is_no_disp = true;
-            }
-          }
-          else
-          {
-            last_time = millis();
-            res_disp();
-            is_no_disp = false;
-          }     
-        }
-        else if(i < 8)
-        {
-          if(millis() - last_time < 1000)
-          {
-            display(P1_time);
-            display((P2_time + (t * conv[i])));
-            res_disp();
-          }
-          else if(millis() - last_time < 1500)
-          {
-            if(!is_no_disp)
-            {
-              no_disp();
-              is_no_disp = true;
-            }
-          }
-          else
-          {
-            last_time = millis();
-            res_disp();
-            is_no_disp = false;
-          }     
+          P1_time -= 10;
         }
         else
         {
-          //time back
-          display(0);
-          display(inc_time);
+          buzzer();
         }
-        
-        if(digitalRead(P1_button) == HIGH && !button_debounce)
-        {
-           if(t > 0)
-              t -= 1;
-           else
-              t = 9;
-           set_bd();
-        }
-        if(digitalRead(P2_button) == HIGH && !button_debounce)
-        {
-          if(t < 9)
-            t += 1;
-          else
-            t = 0;
-          set_bd();
-        }
-        if(digitalRead(pause_button) == HIGH && !button_debounce)
-        {
-          if(i < 4)
-          {
-            P1_time += (t * conv[i]);
-            P2_time += (t * conv[i]);
-            i += 1;
-            t = 0;
-          }
-          else if(i < 8)
-          {
-            P2_time += (t * conv[i]);
-            i+= 1;
-            t = 0;
-          }
-          else if(i >= 8)
-          {
-            inc_time += (t * conv[i]);
-            gameState = SET_READY;
-            set_bd();
-            break;
-          }
-          set_bd();
-        }
-        reset_bd();
-     }
+        Serial.print("\nP1_Time = ");
+        Serial.print(P1_time);
+        set_bd(250);
+      }
+      reset_bd();
+    }
+    //SET INC :00 on both
   }
+  //SET_READY//
   if(gameState == SET_READY)
   {
-     while(true)
-     {
-         display(P1_time);
-         display(P2_time);
-         res_disp();      
-         
-         //hold time waiting
-         if(digitalRead(P1_button) == HIGH)
-         {
-            gameState = P2_TURN;
-            break;
-         }
-         if(digitalRead(P2_button) == HIGH)
-         {
-            gameState = P1_TURN;
-            break;
-         }
-         if(digitalRead(next_button) == HIGH && !button_debounce)
-         {
-            set_bd();
-            gameState = SET_TIME;
-            P1_time = 0;
-            P2_time = 0;
-            break;
-         }
-         reset_bd();
-     }
-     last_time = millis();
-  }
-  if(gameState == P1_TURN)
-  {
-    while(digitalRead(P1_button) != HIGH)
-    {
-      display(P1_time - ((millis() - last_time)/100));
-      display(P2_time);
-      res_disp();
-      if(check_pause())
-      {
-        P1_time -= (millis() - last_time)/100;
-        break;
-      }
-      if(check_timeout(P1_time - ((millis() - last_time)/100)))
-      {
-        P1_time = 0;
-        break;
-      }
-    }
-    if(gameState == P1_TURN)
-    {
-      P1_time -= (millis() - last_time)/100;
-      P1_time += inc_time;
-      last_time = millis();
-      gameState = P2_TURN;
-    }
-  }
-  if(gameState == P2_TURN)
-  {
-    while(digitalRead(P2_button) != HIGH)
+    Serial.println("SET_READY");
+    while(gameState == SET_READY)
     {
       display(P1_time);
-      display(P2_time - ((millis() - last_time)/100));
+      display(P2_time);
       res_disp();
-      if(check_pause())
+  
+      if(digitalRead(P1_button) == HIGH)
       {
-        P2_time -= (millis() - last_time)/100;
-        break;
+        last_time = millis();
+        gameState = P2_TURN;
       }
-      if(check_timeout(P2_time - ((millis() - last_time)/100)))
+      else if(digitalRead(P2_button) == HIGH)
       {
-        P2_time = 0;
-        break;
+        last_time = millis();
+        gameState = P1_TURN;
       }
-    }
-    if(gameState == P2_TURN)
-    {
-      P2_time -= (millis() - last_time)/100;
-      P2_time += inc_time;
-      last_time = millis();
-      gameState = P1_TURN;
     }
   }
+  
+  //P1_TURN//
+  if(gameState == P1_TURN)
+  {
+    Serial.println("P1_TURN");
+    while(digitalRead(P1_button) == LOW)
+    {
+      temp_time = P1_time - ((millis() - last_time)/100);
+      display(temp_time);
+      display(P2_time);
+      res_disp();
+
+      if(check_timeout(temp_time))
+      {
+        Serial.println("Timeout P1");
+        P1_time = 0;
+        gameState = HOLD_TIME;
+        break;
+      }
+      if(digitalRead(pause_button) == HIGH)
+      {
+        gameState = SET_READY;
+        break;
+      }
+    }
+    if(gameState != HOLD_TIME)
+      P1_time -=  (millis() - last_time)/100;
+    if(gameState == P1_TURN)
+    {
+      P1_time += inc_time;
+      gameState = P2_TURN;
+    }
+    last_time = millis();
+  }
+  
+  //P2_TURN//
+  if(gameState == P2_TURN)
+  {
+    Serial.println("P2_TURN");
+    while(digitalRead(P2_button) == LOW)
+    {
+      temp_time = P2_time - ((millis() - last_time)/100);
+      display(P1_time);
+      display(temp_time);
+      res_disp();
+      
+      if(check_timeout(temp_time))
+      {
+        Serial.println("Timeout P2");
+        P2_time = 0;
+        gameState = HOLD_TIME;
+        break;
+      }
+      if(digitalRead(pause_button) == HIGH)
+      {
+        gameState = SET_READY;
+        break;
+      }
+    }
+    if(gameState != HOLD_TIME)
+      P2_time -=  (millis() - last_time)/100;
+    if(gameState == P2_TURN)
+    {
+      P2_time += inc_time;
+      gameState = P1_TURN;
+    }
+    last_time = millis();
+  }
 }
-bool check_pause()
-{
- if(digitalRead(pause_button) == HIGH)
- {
-   gameState = SET_READY;
-   return true;
- }
- return false;
-}
+
 bool check_timeout(int time)
 {
  if(time <= 0)
@@ -300,41 +237,51 @@ bool check_timeout(int time)
 }
 void display(unsigned long int time)
 {
+  int time_to_disp[4] = {0, 0, 0, 0};
+  disp_seg(10);
+  
   time /= 10; //into seconds
   unsigned long int temp_time = 0;
    
   if((temp_time = time / 600) >= 1)
   {
-    disp_seg(temp_time % 10);
-    
+    //disp_seg(temp_time % 10);
+    time_to_disp[0] = temp_time % 10;
     time -= temp_time * 600;
   }
-  else
-  {
-    disp_seg(0);
-  }
-  digitalWrite(deci_pin, HIGH);
-  delay(2);
-  digitalWrite(deci_pin, LOW);
+  
   if((temp_time = time / 60) >= 1)
   {
-    disp_seg(temp_time % 10);
+    //disp_seg(temp_time % 10);
+    time_to_disp[1] = temp_time % 10;
     time -= temp_time * 60;
-  }
-  else
-  {
-    disp_seg(0);
   }
   if((temp_time = time / 10) >= 1)
   {
-    disp_seg((time / 10) % 10); 
+    //disp_seg((time / 10) % 10); 
+    time_to_disp[2] = ((time / 10) % 10);
     time -= temp_time * 10;
   }
-  else
-  {
-    disp_seg(0);
-  }
-  disp_seg((time % 10));
+
+  //disp_seg((time % 10));
+  time_to_disp[3] = time % 10;
+  
+  disp_seg(time_to_disp[3]);
+  delay(2);
+  inc_disp();
+  disp_seg(time_to_disp[2]);
+  delay(2);
+  inc_disp();
+  digitalWrite(deci_pin, HIGH);
+  delay(2);
+  digitalWrite(deci_pin, LOW);
+  disp_seg(time_to_disp[1]);
+  delay(2);
+  inc_disp();
+  disp_seg(time_to_disp[0]);
+  delay(2);
+  inc_disp();
+  disp_seg(10);
 }
 void buzzer()
 {
@@ -351,9 +298,7 @@ void disp_seg(int i)
   digitalWrite(bits_pins[0], bits_bins[i][0]);
   digitalWrite(bits_pins[1], bits_bins[i][1]);
   digitalWrite(bits_pins[2], bits_bins[i][2]);
-  digitalWrite(bits_pins[3], bits_bins[i][3]);
-  delay(2);
-  inc_disp();
+  digitalWrite(bits_pins[3], bits_bins[i][3]); 
 }
 void inc_disp()
 {
@@ -364,28 +309,24 @@ void res_disp()
 {
   digitalWrite(disp_res, HIGH);
   digitalWrite(disp_res, LOW);
+  disp_seg(10);
+  inc_disp();
 }
 void no_disp()
 {
-  res_disp();
-  inc_disp();
-  inc_disp();
-  inc_disp();
-  inc_disp();
-  inc_disp();
-  inc_disp();
-  inc_disp();
-  inc_disp();
-  inc_disp();
+  digitalWrite(disp_res, HIGH);
+  digitalWrite(disp_res, LOW);
+  disp_seg(10);
 }
-void set_bd() //set button debounce
+void set_bd(int dif) //set button debounce
 {
+  bd_dif = dif;
   bd_time = millis();
   button_debounce = true;
 }
 void reset_bd()
 {
-  if(button_debounce && (millis() - bd_time) > 250)
+  if(button_debounce && (millis() - bd_time) > bd_dif)
   {
     button_debounce = false;
   }
